a415a96148402bd2b50737d254e6f4d0dfb49627
James <conwayjw97@gmail.com>
2021-11-29 21:19:12 +0000

    Added season colour coding

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -4,14 +4,25 @@ import "./Canvas.css";
 const black = "rgb(0, 0, 0)";
 const white = "rgb(255, 255, 255)";
 const red = "rgb(255, 0, 0)";
-const feintGrey = "rgba(120, 120, 120, 0.25)";
-const springGreen = "rgba(25, 255, 10, 0.5)";
-const summerYellow = "rgba(240, 255, 10, 0.5)";
-const autumnBrown = "rgba(230, 110, 0, 0.5)";
-const winterBlue = "rgba(10, 255, 250, 0.5)";
+const feintGrey = "rgba(120, 120, 120, 0.3)";
+const springGreen = "rgba(0, 255, 0, 0.5)";
+const summerYellow = "rgba(255, 255, 0, 0.5)";
+const autumnBrown = "rgba(255, 127, 0, 0.5)";
+const winterBlue = "rgba(0, 255, 250, 0.5)";
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+const Summer = Symbol("summer");
+const Autumn = Symbol("autumn");
+const Winter = Symbol("winter");
+const Spring = Symbol("spring");
+
+const monthNames = [
+  "January", "February", "March", "April", "May", "June", "July", "August",
+  "September", "October", "November", "December"
+];
+
+const monthSeasons = [
+  Winter, Winter, Spring, Spring, Spring, Summer, Summer, Summer, Autumn,
+  Autumn, Autumn, Winter
 ];
 
 function degToRad(degrees) {
@@ -40,16 +51,39 @@ function Canvas(props) {
     const date = new Date();
     const days = isLeapYear(date.getFullYear()) ? 366 : 365;
 
+    ctx.fillStyle = white;
+    ctx.beginPath();
+    ctx.moveTo(centreX, centreY);
+    ctx.arc(centreX, centreY, radius, 0, 2 * Math.PI);
+    ctx.lineTo(centreX, centreY);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+
     let radians = - Math.PI / 2;
     ctx.font = "20px Consolas";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    const textOffset = radius / 4;
+    const textOffset = radius / 3;
     for(let i=1; i<13; i+=1) {
       const daysInMonth = new Date(date.getFullYear(), i, 0).getDate();
       const stepSize = ((2 * Math.PI) / days) * daysInMonth;
 
-      ctx.fillStyle = white;
+      switch(monthSeasons[i-1]){
+        case Winter:
+          ctx.fillStyle = winterBlue;
+          break;
+        case Spring:
+          ctx.fillStyle = springGreen;
+          break;
+        case Summer:
+          ctx.fillStyle = summerYellow;
+          break;
+        case Autumn:
+          ctx.fillStyle = autumnBrown;
+          break;
+      }
+
       ctx.beginPath();
       ctx.moveTo(centreX, centreY);
       ctx.arc(centreX, centreY, radius, radians, radians + stepSize, false);
@@ -91,15 +125,15 @@ function Canvas(props) {
     const lineEndX = centreX + radius * Math.cos(radians);
     const lineEndY = centreY - radius * Math.sin(radians);
 
-    ctx.fillStyle = feintGrey;
-    ctx.strokeStyle = feintGrey;
-    ctx.beginPath();
-    ctx.moveTo(centreX, centreY);
-    ctx.arc(centreX, centreY, radius, - startRadians, - startRadians + (dayOfYear * ((2 * Math.PI) / days)), false);
-    ctx.lineTo(centreX, centreY);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
+    // ctx.fillStyle = feintGrey;
+    // ctx.strokeStyle = feintGrey;
+    // ctx.beginPath();
+    // ctx.moveTo(centreX, centreY);
+    // ctx.arc(centreX, centreY, radius, - startRadians, - startRadians + (dayOfYear * ((2 * Math.PI) / days)), false);
+    // ctx.lineTo(centreX, centreY);
+    // ctx.closePath();
+    // ctx.stroke();
+    // ctx.fill();
 
     ctx.lineWidth = 3;
     ctx.strokeStyle = red;
@@ -107,17 +141,6 @@ function Canvas(props) {
     ctx.moveTo(centreX, centreY);
     ctx.lineTo(lineEndX, lineEndY);
     ctx.stroke();
-
-    ctx.fillStyle = feintGrey;
-    ctx.strokeStyle = feintGrey;
-    ctx.beginPath();
-    ctx.moveTo(centreX, centreY);
-    ctx.arc(centreX, centreY, radius, - startRadians, - startRadians + (dayOfYear * ((2 * Math.PI) / days)), false);
-    ctx.lineTo(centreX, centreY);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-
   }, []);
 
   return (

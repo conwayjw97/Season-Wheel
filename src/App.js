483e6f85b09c5a0532bc0dc29c522fa27d842fbd
James <conwayjw97@gmail.com>
2022-01-04 16:39:07 +0000

    Added comparative date input

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import './App.css';
 
 const date = new Date();
 const initialDate = {day: date.getDate(), month: date.getMonth()+1, year: date.getFullYear()};
+const initialCompDate = {disabled: true, day: date.getDate(), month: date.getMonth()+1, year: date.getFullYear()};
 
-function reducer(state, action) {
+function dateReducer(state, action) {
   const elementId = action.nativeEvent.srcElement.id;
   switch (elementId) {
     case 'day':
@@ -25,10 +26,32 @@ function reducer(state, action) {
   }
 }
 
+function compDateReducer(state, action) {
+  const elementId = action.nativeEvent.srcElement.id;
+  switch (elementId) {
+    case 'compDisabled':
+      console.log(action.target.checked)
+      return {...state, disabled: !action.target.checked};
+    case 'compDay':
+      if(action.target.value > 0 && action.target.value < 32){
+        return {...state, day: action.target.value};
+      } else {
+        return {...state};
+      }
+    case 'compMonth':
+      if(action.target.value > 0 && action.target.value < 13){
+        return {...state, month: action.target.value};
+      } else {
+        return {...state};
+      }
+  }
+}
+
 function App() {
   const height = window.innerHeight;
 
-  const [dateState, dateDispatch] = useReducer(reducer, initialDate);
+  const [dateState, dateDispatch] = useReducer(dateReducer, initialDate);
+  const [compDateState, compDateDispatch] = useReducer(compDateReducer, initialCompDate);
   const [updateCount, setUpdateCount] = useState(0);
 
   const handleSave = () => {
@@ -38,7 +61,7 @@ function App() {
   return (
     <div style={{'height':height}} className='App'>
       <Canvas date={dateState} updateCount={updateCount}/>
-      <Settings date={dateState} handleDateChange={dateDispatch} handleSave={handleSave}/>
+      <Settings date={dateState} compDate={compDateState} handleDateChange={dateDispatch} handleCompDateChange={compDateDispatch} handleSave={handleSave}/>
     </div>
   );
 }
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -15,8 +15,10 @@ function Settings(props) {
       <div className="settingsMenu" style={{width: showing ? "500px" : "0"}}>
         <button className="button closeButton" onClick={handleButtonClick}>&times;</button>
 
+        <label className="heading">
+         Date
+        </label>
         <label className="" style={{padding: "1em 0", display: "block"}}>
-         Date:
           <input id="day" type="number" style={{marginLeft: "0.25em", width: "45px", fontSize: "28px"}} value={props.date.day} onChange={props.handleDateChange}/>
           /
           <input id="month" type="number" style={{width: "45px", fontSize: "28px"}} value={props.date.month} onChange={props.handleDateChange}/>
@@ -24,6 +26,21 @@ function Settings(props) {
           {props.date.year}
         </label>
 
+        <hr/>
+
+        <label className="heading">
+         Comparative Date
+         <input id="compDisabled" type="checkbox" style={{marginLeft: "1em", transform: "scale(1.5)"}} checked={!props.compDate.disabled} onChange={props.handleCompDateChange}/>
+        </label>
+        <label className="" style={{padding: "1em 0", display: "block"}}>
+          <input id="compDay" type="number" style={{marginLeft: "0.25em", width: "45px", fontSize: "28px"}} disabled={props.compDate.disabled} value={props.compDate.day} onChange={props.handleCompDateChange}/>
+          /
+          <input id="compMonth" type="number" style={{width: "45px", fontSize: "28px"}} disabled={props.compDate.disabled} value={props.compDate.month} onChange={props.handleCompDateChange}/>
+          /
+          {props.date.year}
+        </label>
+
+        <hr/>
         <div className="separator" />
 
         <button className="save" onClick={props.handleSave}>Save</button>
